a035713fca8b3f695a5b8982a6c3e91479d9f9dc
Rodricancio8 <rodri.cancio@hotmail.com>
2023-08-31 17:59:35 +0200

    footer

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -66,36 +66,5 @@ function Navbar() {
     </nav>
   );
 }
-// <nav>
-//   <Link to="/">
-//     <button>Home</button>
-//   </Link>
-
-//   {isLoggedIn && (
-//     <>
-//       <button onClick={logOutUser}>Logout</button>
-
-//       <Link to="/profile">
-//         <button>Profile</button>
-//         {/* <img src="https://picsum.photos/id/402/200/300" style={{ width: 50, height: 50, borderRadius: 25}} alt="profile" /> */}
-//       </Link>
-
-//       <span>{user && user.name}</span>
-//     </>
-//   )}
-
-//   {!isLoggedIn && (
-//     <>
-//       <Link to="/signup">
-//         {" "}
-//         <button>Sign Up</button>{" "}
-//       </Link>
-//       <Link to="/login">
-//         {" "}
-//         <button>Login</button>{" "}
-//       </Link>
-//     </>
-//   )}
-// </nav>
 
 export default Navbar;
