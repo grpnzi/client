09984f9e72bfdb08aac510ec66f439f9b6dab7cb
Rodricancio8 <rodri.cancio@hotmail.com>
2023-09-04 15:09:11 +0200

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,8 +5,7 @@ import { AuthContext } from "../../context/auth.context";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'typeface-russo-one';
 import 'typeface-montserrat'
-import { Button } from "react-bootstrap";
-import { MdAddShoppingCart } from "react-icons/md";
+import { MDBIcon } from 'mdb-react-ui-kit';
 
 
 
@@ -35,13 +34,13 @@ function Navbar() {
 
         `}
       </style>
-      
+
       <Link style={{ fontFamily: 'Russo One' }} className="navbar-brand ml-4" to="/">
         <div className=" d-flex align-items-center mr-2">
           <p className="mb-0 mr-23">wildXperience</p>
           <img src={process.env.PUBLIC_URL + '/logo 3.png'} alt="Mi Logo" className="mr-2" style={{ width: '60px' }} />
         </div>
-      </Link> 
+      </Link>
       <button
         className="navbar-toggler ml-auto"
         type="button"
@@ -50,19 +49,22 @@ function Navbar() {
         aria-controls="navbarNav"
         aria-expanded="false"
         aria-label="Toggle navigation"
-        >
+      >
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse ml-auto justify-content-end mr-4" id="navbarNav">
         <ul className="navbar-nav">
           {isLoggedIn && (
             <>
+            <Link to="/cart" className="mr-2">
+                <MDBIcon icon="cart-plus" className="me-2 mt-2 p-1" />
+              </Link>
               <li className="nav-item">
-                <Link style={{ fontFamily: 'Share', color:'black' }} className="nav-link font-weight-bold" to="/profile">
-                {user && user.name}
+                <Link style={{ fontFamily: 'Share', color: 'black' }} className="nav-link font-weight-bold" to="/profile">
+                  {user && user.name}
                 </Link>
               </li>
-              <li style={{ fontFamily: 'Share', color:'black' }} className="nav-item font-weight-bold">
+              <li style={{ fontFamily: 'Share', color: 'black' }} className="nav-item font-weight-bold">
                 <button className="nav-link" onClick={logOutUser}>
                   Logout
                 </button>
@@ -71,17 +73,13 @@ function Navbar() {
           )}
           {!isLoggedIn && (
             <>
-            <Link to="/cart" className="btn" style={{ padding: '5px 10px' }}>
-  <MdAddShoppingCart className="me-2 bg-warning" style={{ fontFamily: 'Share', fontSize: '24px' }} />
-  Add to Cart
-</Link>
               <li className="nav-item ml-auto d-flex justify-content-end">
-                <Link style={{ color: 'black'}} className="nav-link font-weight-bold share-font" to="/signup">
+                <Link style={{ color: 'black' }} className="nav-link font-weight-bold share-font" to="/signup">
                   Sign Up
                 </Link>
               </li>
               <li className="nav-item ml-auto d-flex justify-content-end">
-                <Link style={{ color: 'black'}}  className="nav-link share-font" to="/login">
+                <Link style={{ color: 'black' }} className="nav-link share-font" to="/login">
                   Login
                 </Link>
               </li>
