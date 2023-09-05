37ab774e932bab00a17cc7970ba242208813c1f1
German Panzardi <gerpanza@gmail.com>
2023-09-05 10:36:35 +0200

    cart fix

--- src/components/CartDetails/Cart.jsx
+++ src/components/CartDetails/Cart.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { CartContext } from "../../context/cart.context";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import {
   MDBCard,
   MDBCardBody,
@@ -87,49 +87,9 @@ function Cart() {
                       )
                     })}
 
-          <MDBCol lg="5">
-                <MDBCard className="bg-primary text-white rounded-3">
-                  <MDBCardBody>
-                    <div className="d-flex justify-content-between align-items-center mb-4">
-                      <MDBTypography tag="h5" className="mb-0">
-                        Card details
-                      </MDBTypography>
-                      <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp"
-                        fluid className="rounded-3" style={{ width: "45px" }} alt="Avatar" />
-                    </div>
 
-                    <p className="small">Card type</p>
-                    <a href="#!" type="submit" className="text-white">
-                      <MDBIcon fab icon="cc-mastercard fa-2x me-2" />
-                    </a>
-                    <a href="#!" type="submit" className="text-white">
-                      <MDBIcon fab icon="cc-visa fa-2x me-2" />
-                    </a>
-                    <a href="#!" type="submit" className="text-white">
-                      <MDBIcon fab icon="cc-amex fa-2x me-2" />
-                    </a>
-                    <a href="#!" type="submit" className="text-white">
-                      <MDBIcon fab icon="cc-paypal fa-2x me-2" />
-                    </a>
+                  </MDBCol>
 
-                    <form className="mt-4">
-                      <MDBInput className="mb-4" label="Cardholder's Name" type="text" size="lg"
-                        placeholder="Cardholder's Name" contrast />
-
-                      <MDBInput className="mb-4" label="Card Number" type="text" size="lg"
-                        minLength="19" maxLength="19" placeholder="1234 5678 9012 3457" contrast />
-
-                      <MDBRow className="mb-4">
-                        <MDBCol md="6">
-                          <MDBInput className="mb-4" label="Expiration" type="text" size="lg"
-                            minLength="7" maxLength="7" placeholder="MM/YYYY" contrast />
-                        </MDBCol>
-                        <MDBCol md="6">
-                          <MDBInput className="mb-4" label="Cvv" type="text" size="lg" minLength="3"
-                            maxLength="3" placeholder="&#9679;&#9679;&#9679;" contrast />
-                        </MDBCol>
-                      </MDBRow>
-                    </form>
                   <MDBCol lg="6">
                     <MDBCard className="bg-primary text-white rounded-3 w-100">
                       <MDBCardBody className="w-100">
@@ -201,7 +161,6 @@ function Cart() {
           </MDBCol>
         </MDBRow>
       </MDBContainer>
-
     </section>
   );
 }
