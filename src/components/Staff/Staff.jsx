be05edd29054a4a7f69ecbb1b980a7beb038c4b3
JoanLopezIT <joan.lopez.job@gmail.com>
2023-09-06 18:05:43 +0200

    responsive

--- src/components/Staff/Staff.jsx
+++ src/components/Staff/Staff.jsx
@@ -25,51 +25,137 @@ function Staff() {
 
 
     return (
-        <div className="row featurette mt-2 mx-0">
-          <div className="col-md-7">
-            <h2 style={{ fontFamily: 'Russo One', fontSize: '25px' }} className="featurette-heading">
-              Crew & Staff
-            </h2>
-            <p style={{ fontFamily: 'Share', fontSize: '20px' }} className="lead">
-              Experts in everything from sports to culture and history, the members of our team are always eager to share their infectious passion for traveling and enjoying new experiences. Add in professional hotel staff and a seasoned crew, and you will be more than comfortable during your adventure.
-            </p>
-          </div>
-          <div className="col-md-5 mt-5 d-flex">
-            {guides ? (
-              guides.map((guide) => {
-                return (
-                  <div key={guide.id} style={{ marginRight: '20px' }}>
-                    <Card className="bg-dark text-white" style={{ width: '250px', height: '250px' }}>
-                      <Card.Img
-                        src={guide.img}
-                        alt="Card image"
-                        className="img-thumbnail"
-                        style={{ objectFit: 'cover', height: '100%', width: '100%' }}
-                      />
-                      <div
-                        className="card-img-overlay d-flex flex-column justify-content-end align-items-start"
-                        style={{
-                          background: `linear-gradient(to top, rgba(0, 0, 0, 0.4) 20%, transparent 100%)`,
-                          padding: '20px',
-                          height: '100%',
-                        }}
-                      >
-                        <Card.Title style={{ color: 'white' }}>{guide.name} </Card.Title>
-                        <Card.Text style={{ color: 'white' }}>{guide.occupation} </Card.Text>
-                      </div>
-                    </Card>
-                  </div>
-                );
-              })
-            ) : (
-              <p>Loading...</p>
-            )}
-          </div>
-        </div>
-      );
+<div className="container mt-5">
+  <div className="row featurette">
+    <div className="col-md-5">
+      <h2 style={{ fontFamily: 'Russo One', fontSize: '25px' }} className="featurette-heading">
+        Crew & Staff
+      </h2>
+      <p style={{ fontFamily: 'Share', fontSize: '20px' }} className="lead">
+        Experts in everything from sports to culture and history, the members of our team are always eager to share their infectious passion for traveling and enjoying new experiences. Add in professional hotel staff and a seasoned crew, and you will be more than comfortable during your adventure.
+      </p>
+    </div>
+    <div className="col-md-6 mt-5 d-flex flex-wrap">
+      {guides ? (
+        guides.map((guide) => {
+          return (
+            <div key={guide.id} className="mb-4 col-md-4 col-lg-3" style={{ maxWidth: '250px', minWidth: '200px', margin: '0 15px' }}>
+              <Card className="bg-dark text-white">
+                <Card.Img
+                  src={guide.img}
+                  alt="Card image"
+                  className="img-thumbnail img-fluid"
+                  style={{ objectFit: 'cover', height: '250px', width: '100%', maxWidth: '100%' }} // Set maxWidth here
+                />
+                <div
+                  className="card-img-overlay d-flex flex-column justify-content-end align-items-start"
+                  style={{
+                    background: `linear-gradient(to top, rgba(0, 0, 0, 0.4) 20%, transparent 100%)`,
+                    padding: '20px',
+                    height: '100%',
+                  }}
+                >
+                  <Card.Title style={{ color: 'white' }}>{guide.name}</Card.Title>
+                  <Card.Text style={{ color: 'white' }} className="text-left">{guide.occupation}</Card.Text>
+                </div>
+              </Card>
+            </div>
+          );
+        })
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  </div>
+</div>
+
+
+    );
+    
+
+    
+    
       
 }
 
 export default Staff;
 
 
+
+
+
+
+
+{/* <div>
+<div className="container-fluid mt-5 -5">
+<div className="row d-flex experienceContainer rounded p-4 mx-4 customCss background">
+    <div className="col-md-6">
+        <div className="d-flex align-items-center justify-content-center">
+            <img
+                src={experience.imageUrl}
+                alt={experience.title}
+                className="img-fluid imgDescription rounded"
+                style={{ maxWidth: '100%', height: 'auto' }}
+            />
+        </div>
+    </div>
+    <div className="col-md-6 experienceDiv">
+        <h1 className="mt-4">{experience.title}</h1>
+        <p className="lead">{experience.description}</p>
+        <p className="mt-4" style={{ fontFamily: 'Share', fontSize: '19px' }}>
+            Price: ${experience.price}
+            <button
+                className="text-center btn btn-sm btn-dark rounded border border-warning"
+                style={{ width: '130px', maxHeight: '35px', fontFamily: 'Share', fontSize: '14px' }}
+                onClick={() => { cartUpdate(experience) }}
+            >
+                Purchase
+            </button>
+        </p>
+    </div>
+</div>
+</div>
+<div className="container mt-5">
+<div className="row featurette">
+  <div className="col-md-5">
+    <h2 style={{ fontFamily: 'Russo One', fontSize: '25px' }} className="featurette-heading">
+      Crew & Staff
+    </h2>
+    <p style={{ fontFamily: 'Share', fontSize: '20px' }} className="lead">
+      Experts in everything from sports to culture and history, the members of our team are always eager to share their infectious passion for traveling and enjoying new experiences. Add in professional hotel staff and a seasoned crew, and you will be more than comfortable during your adventure.
+    </p>
+  </div>
+  <div className="col-md-6 mt-5 d-flex flex-wrap">
+    {guides ? (
+      guides.map((guide) => {
+        return (
+          <div key={guide.id} className="mb-4 col-md-4 col-lg-3" style={{ maxWidth: '200px', minWidth: '100px', margin: '0 10px' }}>
+            <Card className="bg-dark text-white">
+              <Card.Img
+                src={guide.img}
+                alt="Card image"
+                className="img-thumbnail"
+                style={{ objectFit: 'cover', height: '250px', width: '100%' }}
+              />
+              <div
+                className="card-img-overlay d-flex flex-column justify-content-end align-items-start"
+                style={{
+                  background: `linear-gradient(to top, rgba(0, 0, 0, 0.4) 20%, transparent 100%)`,
+                  padding: '20px',
+                  height: '100%',
+                }}
+              >
+                <Card.Title style={{ color: 'white' }}>{guide.name}</Card.Title>
+                <Card.Text style={{ color: 'white' }} className="text-left">{guide.occupation}</Card.Text>
+              </div>
+            </Card>
+          </div>
+        );
+      })
+    ) : (
+      <p>Loading...</p>
+    )}
+  </div>
+</div>
+</div>
+</div> */}
